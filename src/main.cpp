2de27e0bb2169a99f05dc994062ba1a5410351e7
Waterloop <teamwaterloop@gmail.com>
2019-10-05 12:55:08 -0400

    Changed build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <canbus.h>
 #include <task_manager.h>
+#include <unistd.h>
 
 using namespace wlp;
 
 int main() {
+
+	dup2(1, 2); // Redirects stderr to stdout
 
 	canbus bus("can0");
 
@@ -23,10 +26,12 @@ int main() {
 		for(int i = 0; i < 8; ++i) {
 			printf("%02x ", data[i]);
 		}*/
-		printf("\n");
+		printf("Send can\n");
+		fprintf(stderr, "Send can err\n");
 		data[0] = 16;
 		data[1] = 12;
 		bus.send(45, data, 2);
+		sleep(1);
 	}
 
 	return 0;
---
+++ src/packet_decoder.cpp
@@ -0,0 +1,3 @@
+(int acc_x, int acc_y, int acc_z, int temp){
+
+}
