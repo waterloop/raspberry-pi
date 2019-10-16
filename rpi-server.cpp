78988af19894897c8aba29a5fc39c57474a10b7b
Sergiu Pocol <sergiu.p@waterloop.ca>
2019-10-15 23:19:07 -0400

    included a necessary library

--- rpi-server.cpp
+++ rpi-server.cpp
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <strings.h>
+#include <unistd.h>
 
 
 // Prints msg to stderr and exits
@@ -78,12 +79,20 @@ int main(int argc, char *argv[]) {
     newsockfd = accept(sockfd, (struct sockaddr *) &desktop_addr, (socklen_t *) &clilen);
     if (newsockfd < 0) error("ERROR on accept");
 
+    bzero(buffer,256);
+    n = read(newsockfd,buffer,255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n", buffer);
+
+    n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+
+    return 0;
 
 
 
-
-    
-
+    // https://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
+    // use the tutorial above to handle threading for listening to BOTH RPis
 
 }
 
